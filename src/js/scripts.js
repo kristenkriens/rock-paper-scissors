6a714ccf37af5c27dfe402695addababf3215e0f
Kristen Kriens <kristen.kriens@gmail.com>
2017-10-04 11:22:15 -0400

    Refactor js to cut down on code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,6 +8,7 @@ $('input[type="radio"]').on('click', function() {
   var computerChoice = Math.random();
   var userChoice = $('input[type="radio"]:checked').val();
   var choices = '';
+  var bothChoices = [];
   var outcome = '';
 
   if (computerChoice < 0.2) {
@@ -24,32 +25,27 @@ $('input[type="radio"]').on('click', function() {
 
   choices = `The computer chose ${computerChoice} and you chose ${userChoice}`;
 
-  if ((userChoice === 'Scissors' || computerChoice === 'Scissors') && (userChoice === 'Paper' || computerChoice === 'Paper')) {
-    outcome = 'Scissors cuts Paper';
-  } else if ((userChoice === 'Paper' || computerChoice === 'Paper') && (userChoice === 'Rock' || computerChoice === 'Rock')) {
-    outcome = 'Paper covers Rock';
-  } else if ((userChoice === 'Rock' || computerChoice === 'Rock') && (userChoice === 'Lizard' || computerChoice === 'Lizard')) {
-    outcome = 'Rock crushes Lizard';
-  } else if ((userChoice === 'Lizard' || computerChoice === 'Lizard') && (userChoice === 'Spock' || computerChoice === 'Spock')) {
-    outcome = 'Lizard poisons Spock';
-  } else if ((userChoice === 'Spock' || computerChoice === 'Spock') && (userChoice === 'Scissors' || computerChoice === 'Scissors')) {
-    outcome = 'Spock smashes Scissors';
-  } else if ((userChoice === 'Scissors' || computerChoice === 'Scissors') && (userChoice === 'Lizard' || computerChoice === 'Lizard')) {
-    outcome = 'Scissors decapitates Lizard';
-  } else if ((userChoice === 'Lizard' || computerChoice === 'Lizard') && (userChoice === 'Paper' || computerChoice === 'Paper')) {
-    outcome = 'Lizard eats Paper';
-  } else if ((userChoice === 'Paper' || computerChoice === 'Paper') && (userChoice === 'Spock' || computerChoice === 'Spock')) {
-    outcome = 'Paper disproves Spock';
-  } else if ((userChoice === 'Spock' || computerChoice === 'Spock') && (userChoice === 'Rock' || computerChoice === 'Rock')) {
-    outcome = 'Spock vaporizes Rock';
-  } else if ((userChoice === 'Rock' || computerChoice === 'Rock') && (userChoice === 'Scissors' || computerChoice === 'Scissors')) {
-    outcome = 'Rock crushes Scissors';
-  } else {
-    outcome = 'You chose the same as the computer';
+  bothChoices.push(computerChoice, userChoice);
+
+  function showOutcome(choice1, action, choice2) {
+    if(bothChoices.includes(choice1) && bothChoices.includes(choice2)) {
+      outcome = `${choice1} ${action} ${choice2}`;
+    }
   }
 
+  showOutcome('Scissors', 'cuts', 'Paper');
+  showOutcome('Paper', 'covers', 'Rock');
+  showOutcome('Rock', 'crushes', 'Lizard');
+  showOutcome('Lizard', 'poisons', 'Spock');
+  showOutcome('Spock', 'smashes', 'Scissors');
+  showOutcome('Scissors', 'decapitates', 'Lizard');
+  showOutcome('Lizard', 'eats', 'Paper');
+  showOutcome('Paper', 'disproves', 'Spock');
+  showOutcome('Spock', 'vaporizes', 'Rock');
+  showOutcome('Rock', 'crushes', 'Scissors');
+
   if (computerChoice === userChoice) {
-    $('.rules-score__item--text h3').text(`${choices}. ${outcome}, so it's a tie!`);
+    $('.rules-score__item--text h3').text(`${choices}. It's a tie!`);
     score.tie += 1;
     $('tr:last-of-type td:nth-of-type(3)').text(score.tie);
   } else if ((userChoice === 'Scissors' && computerChoice === 'Paper') || (userChoice === 'Paper' && computerChoice === 'Rock') || (userChoice === 'Rock' && computerChoice === 'Lizard') || (userChoice === 'Lizard' && computerChoice === 'Spock') || (userChoice === 'Spock' && computerChoice === 'Scissors') || (userChoice === 'Scissors' && computerChoice === 'Lizard') || (userChoice === 'Lizard' && computerChoice === 'Paper') || (userChoice === 'Paper' && computerChoice === 'Spock') || (userChoice === 'Spock' && computerChoice === 'Rock') || (userChoice === 'Rock' && computerChoice === 'Scissors')) {
